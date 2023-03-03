3428f77d32e8823c9c256a043ec9b6203a06b8a3
Karl Henselin <karl.henselin@flexagon.com>
2023-03-03 14:59:50 -0600

    Update SpringBootAppJobSolutionsXprCanadaApplicationTests.java

--- src/test/java/com/tech_interview/quebec_canada/walid_zhani/solutions_xpr/SpringBootAppJobSolutionsXprCanadaApplicationTests.java
+++ src/test/java/com/tech_interview/quebec_canada/walid_zhani/solutions_xpr/SpringBootAppJobSolutionsXprCanadaApplicationTests.java
@@ -1,15 +1,15 @@
 package com.tech_interview.quebec_canada.walid_zhani.solutions_xpr;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class SpringBootAppJobSolutionsXprCanadaApplicationTests {
 
     @Test
     void contextLoads() {
-        Assert.assertEquals("Did not pass.", true, false);
+        assertEquals(true, false);
     }
 
 }
